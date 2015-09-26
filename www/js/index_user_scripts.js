3db54459784b907b597b57108f766289a0a583ad
Simon <sim@Sim-MBP.local>
2015-09-27 00:10:53 +0200

    first commit

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,16 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers
+ */
+ function register_event_handlers()
+ {
+
+
+     /* button  Button */
+
+
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
